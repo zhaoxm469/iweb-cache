3f196f439ee4fc4f3b6ba71ab5fd024254d5f81b
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-28 17:29:58 +0800

    feat(type): 增加注释

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,41 +1,71 @@
-export type drivingType = 'localStorage' | 'sessionStorage';
+export type storageType = 'localStorage' | 'sessionStorage';
 export type cacheGetKeyValueType = string | object | boolean;
 export type ICacheDataList = cacheGetKeyValueType[];
+export type cacheNotValueType = false;
 
 export interface IglobalVariableData {
     [key: string]: ICacheData;
 }
 
 export interface IcacheOptions {
-    // 使用哪种方式进行数据存储
-    storageType: drivingType;
-    // 储存数据的前缀
-    prefix: string;
-    // 全局过期时间
+    /**
+     * @description: 使用哪种方式进行数据存储
+     * @type { string }
+     * @default localStorage
+     */
+    storageType?: storageType;
+    /**
+     * @description: 储存数据的前缀
+     */
+    prefix?: string;
+    /**
+     * @description: 全局过期时间
+     */
     expires?: number;
 }
 
 export interface ICacheData {
-    // 存储的数据
+    /**
+     * @description: 存储的数据
+     */
     value: string;
-    // 单个数据的过期时间
+    /**
+     * @description: 单个数据的过期时间
+     */
     expires?: number;
 }
 
 export interface IwebCache {
-    // 设置缓存
-    save(key: string, value: any, expires?: number): boolean;
-    // 覆盖缓存数据，但不更新过期时间
+    appData: IglobalVariableData;
+    /**
+     * @description: 设置缓存
+     */
+    save<T = any>(key: string, value: T, expires?: number): boolean;
+    /**
+     * @description: 覆盖缓存数据，但不更新过期时间
+     */
     cover(key: string, value: any): void;
-    // 根据指定key获取缓存数据
-    get(key: string): cacheGetKeyValueType;
-    // 根据key获取多条缓存数据
-    getAll(...arg: string[]): ICacheDataList;
-    // 获取缓存内容的大小
+    /**
+     * @description: 根据指定key获取缓存数据
+     */
+    // get<T = any>(key: string): T | cacheNotValueType;
+    /**
+     * @description: 获取多条缓存数据
+     * @example
+     * const [ token,token1 ] = cache.getAll('token','token1')
+     */
+    getAll(...arg: string[]): [any];
+    /**
+     * @description: 获取缓存内容的大小
+     */
     getCacheSize(): number | boolean;
-    // 根据key删除指定缓存数据
+    /**
+     * @description: 根据key删除指定缓存数据
+     */
     del(key: string): boolean;
-    // 清空全部缓存数据
+    /**
+     * @description: 清空全部缓存数据
+     */
     clear(): void;
 }
 
