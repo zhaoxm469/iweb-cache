48cebe95421c043309d2efa751f7d8edba0aab69
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-29 17:37:38 +0800

    fix: 格式化

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-//   testEnvironment: 'node',
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	//   testEnvironment: 'node',
 };
